4123981939b2ea4ff7e00f5ce919764a840b1529
benpariswork <88748300+benpariswork@users.noreply.github.com>
2022-11-19 02:20:44 -0800

    updated about me text

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -6,7 +6,7 @@ export default {
 				{
 					id: 1,
 					bio:
-						'Growing up in Seattle gave me the opportunity to build a seemingly endless toolkit, perfectly tailored for web development and security. I have formal training from University of Oregon Continuin Education, in the form of a Certificate in Cybersecurity. I have also been learning full stack web development for close to two years. My mentor for web development has guided me throught the process of understanding front end and back end and all inbetween. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ill uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). ',
+						'Growing up in Seattle gave me the opportunity to build a seemingly endless toolkit, perfectly tailored for web development and security. I have formal training from University of Oregon Continuin Education, in the form of a Certificate in Cybersecurity. I have also been learning full stack web development for close to two years. My mentor for web development has guided me through the process of understanding front end and back end and all that lies in between.',
 				},
 				{
 					id: 2,
