ff64d909f0147a1047d323c28116a3d38479b7e3
benpariswork <88748300+benpariswork@users.noreply.github.com>
2022-11-19 02:23:53 -0800

    testing .txt resume for download

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -39,8 +39,8 @@ export default {
 			Born and raised in Seattle, living in San Diego. Currently working as a freelance web developer and co founder of an online marketplace. In my day to day i have a lot of priorities, code and security are right up at the top.			</p>
 			<div class="flex justify-center sm:block">
 				<a
-					download="Benjamin-Paris-Resume.pdf"
-					href="/files/Benjamin-Paris-Resume.pdf"
+					download="Benjamin-Paris-Resume.txt"
+					href="/files/Benjamin-Paris-Resume.txt"
 					class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-emerald-600 text-gray-500 hover:text-white duration-500"
 					aria-label="Download Resume"
 				>
